9faab87e8c26863cea97eca0a73e522e2d0146f7
정찬영 <jcy0342@gmail.com>
2024-03-20 09:21:50 +0900

    refactor: JPA Auditing 수정자 아이디 시큐리티 컨텍스트에서 가져오도록 변경

--- src/main/java/com/example/studyroom/config/JpaConfig.java
+++ src/main/java/com/example/studyroom/config/JpaConfig.java
@@ -1,9 +1,13 @@
 package com.example.studyroom.config;
 
+import com.example.studyroom.security.CustomUserDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
@@ -13,6 +17,11 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<Long> auditorAware(){
-        return () -> Optional.of(1L);   // TODO: 3/17/24 시큐리티 인증 후 구현
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(CustomUserDetails.class::cast)
+                .map(CustomUserDetails::getId);
     }
 }
